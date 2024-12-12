1b8c29870e5bba6c4b0a1fe16c436e0dfa7a5fd5
ukyen <wucean@gmail.com>
2024-12-12 23:12:02 +0000

    Escape tag directive

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,7 +1,7 @@
 pub(crate) mod cfn;
 pub(crate) mod config;
 pub(crate) mod iac;
-use marked_yaml::{parse_yaml, Node, Span};
+use marked_yaml::{Node, Span, parse_yaml};
 use std::fs;
 
 pub trait LineMarker {
@@ -35,6 +35,16 @@ impl LineMarker for YamlLineMarker {
 
 pub(crate) fn get_yaml_line_marker(template: &str) -> Result<YamlLineMarker, std::io::Error> {
     let doc = fs::read_to_string(template)?;
-    let node = parse_yaml(0, &doc).expect("Failed to parse YAML");
+    let preprocessed_doc = preprocess_yaml(&doc);
+    let node = parse_yaml(0, &preprocessed_doc).expect("Failed to parse YAML");
     Ok(YamlLineMarker::new(node))
+}
+fn preprocess_yaml(doc: &str) -> String {
+    let mut lines: Vec<String> = Vec::new();
+    for line in doc.lines() {
+        // Replace `!` with `###` to avoid parsing issues with `!` in the YAML
+        let cleaned_line = line.replace("!", "###");
+        lines.push(cleaned_line);
+    }
+    lines.join("\n")
 }
