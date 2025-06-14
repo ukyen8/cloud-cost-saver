d6f99a297df7296d54906edfe8eaa0e6b2c1f484
ukyen <wucean@gmail.com>
2025-06-14 08:11:45 +0100

    Add github action

---
+++ .github/actions/cloud_cost_saver/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Positional arguments from action.yml
+TEMPLATE="$1"
+ENVIRONMENT="$2"
+SAMCONFIG="$3"
+CONFIG="$4"
+
+ARGS="--template \"$TEMPLATE\" --environment \"$ENVIRONMENT\" --config \"$CONFIG\""
+
+if [ -n "$SAMCONFIG" ]; then
+  ARGS="$ARGS --samconfig \"$SAMCONFIG\""
+fi
+
+# Execute the Rust binary with the constructed arguments
+exec /action/target/release/cloud_cost_saver $ARGS
